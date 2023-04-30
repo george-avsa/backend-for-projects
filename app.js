a7f1c8b830ac6b478c75f4079749ac8fc9fb5ae3
Георгий Авсаджанишвили <gogiavsa@Georgijs-MacBook-Air.local>
2023-04-30 19:45:02 +0300

    some changes

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const mysql = require("mysql2");
-const app = express();
-const port = 3000;
+    const express = require("express");
+    const mysql = require("mysql2");
+    const app = express();
+    const port = 3000;
 
-const kek = new Promise((resolve, reject) => {
-    const connection = mysql.createConnection({
-        host: "92.53.96.116",
-        user: "cs71915_invoice",
-        database: "cs71915_invoice",
-        password: "Alp22212"
-    })
-    
-    connection.query(
-        'SELECT * FROM `invoices`',
-        function(err, results, fields) {
-            connection.close();
-            resolve(results); 
+    const kek = new Promise((resolve, reject) => {
+        const connection = mysql.createConnection({
+            host: "92.53.96.116",
+            user: "cs71915_invoice",
+            database: "cs71915_invoice",
+            password: "Alp22212"
+        })
+        
+        connection.query(
+            'SELECT * FROM `invoices`',
+            function(err, results, fields) {
+                connection.close();
+                resolve(results); 
+            }
+            )
+    });
+
+    app.get('/', (req, res) => {
+        const email = req.params.email;
+        const pass = req.params.pass;
+        try {
+            kek.then(data => res.send(JSON.stringify(data)));
+        } catch (e) {
+            console.log('something went wrong!');
         }
-        )
-});
+    })
 
-app.get('/', (req, res) => {
-    const email = req.params.email;
-    const pass = req.params.pass;
-    try {
-        kek.then(data => res.send(JSON.stringify(data)));
-    } catch (e) {
-        console.log('something went wrong!');
-    }
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    });
